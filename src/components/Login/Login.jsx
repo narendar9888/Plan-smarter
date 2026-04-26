b94bf2747b217b8cdce576ecfde3137b00842a80
Narendar kumar <229602725+narendar9888@users.noreply.github.com>
2026-04-27 02:13:29 +0530

    React Setup

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import "./Login.css";
+
+function Login({ onLogin }){
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("")
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+    
+
+    if (!name || !email || !password) {
+        alert("Please fill all fields")
+        return;
+    }
+
+    onLogin({
+        name,
+        email,
+    });
+}
+
+    return (
+        <div className="login-page">
+            <div className="login-card">
+                <h1>Your World</h1>
+                <h2>Welcome Back</h2>
+                <p>Turn your goals into reality by staying consistent and focused.</p>
+
+                <form onSubmit={handleLogin}>
+                    <input 
+                    type="text" 
+                    placeholder="Enter Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    />
+
+                    <br />
+
+                    <input 
+                    type="email" 
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <br />
+
+                    <input 
+                    type="password" 
+                    placeholder="Enter Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <br />
+
+                    <button type="submit">Login</button>
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
+
+
